42a7382ea40845f95119256a38fe8d1e139e3384
arunrs-workspace <arunrs.workspace@gmail.com>
2025-01-25 23:34:40 +0530

    New Changes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3060;
+
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
+
+// Route to serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port:${port}`);
+});
+
